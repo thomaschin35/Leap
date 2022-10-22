3610872f56d072e286ef7a999db9636ed811eafc
Aria Sharma <ariasharmagt@gmail.com>
2022-10-22 15:14:39 -0400

    Created dashboard page file

---
+++ Leap/Views/DashboardPage.swift
@@ -0,0 +1,20 @@
+//
+//  DashboardPage.swift
+//  Leap
+//
+//  Created by Aria Sharma on 10/22/22.
+//
+
+import SwiftUI
+
+struct DashboardPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DashboardPage_Previews: PreviewProvider {
+    static var previews: some View {
+        DashboardPage()
+    }
+}
