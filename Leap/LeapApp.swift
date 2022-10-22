816375c64819fe8a8607c7944d9b25bf9aaad5b7
Thomas Chin <thomas081904@gmail.com>
2022-10-22 01:38:13 -0400

    Initial Commit

---
+++ Leap/LeapApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeapApp.swift
+//  Leap
+//
+//  Created by Thomas Chin on 10/22/22.
+//
+
+import SwiftUI
+
+@main
+struct LeapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
