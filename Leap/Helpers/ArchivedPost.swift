ad4459e795c4fdfaa1180912d225c46075a0c19b
Aria Sharma <ariasharmagt@gmail.com>
2022-10-23 03:57:37 -0400

    add challenge page done

--- Leap/Helpers/ArchivedPost.swift
+++ Leap/Helpers/ArchivedPost.swift
@@ -15,7 +15,7 @@ struct ArchivedPost {
 extension ArchivedPost {
     static let sampleData: [ArchivedPost] = [
         ArchivedPost(task: "Lorem ipsum dolor sit amet.", body: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."),
-        ArchivedPost(task: "Metus aliquam eleifend mi in nulla posuere.", body: "Feugiat in fermentum posuere urna nec tincidunt praesent semper. Sagittis purus sit amet volutpat consequat mauris nunc congue nisi.")
+        ArchivedPost(task: "Metus aliquam eleifend mi in nulla posuere.", body: "Feugiat in fermentum posuere urna nec tincidunt praesent semper. Sagittis purus sit amet volutpat consequat mauris nunc congue nisi."),
         ArchivedPost(task: "Lectus sit amet est placerat in egestas erat imperdiet.", body: "Condimentum id venenatis a condimentum vitae sapien pellentesque habitant. Sit amet volutpat consequat mauris nunc. Cras semper auctor neque vitae tempus quam.")
     ]
 }
--- Leap/Views/AddChallenge.swift
+++ Leap/Views/AddChallenge.swift
@@ -8,8 +8,28 @@
 import SwiftUI
 
 struct AddChallenge: View {
+    @Environment (\.managedObjectContext) var managedObjectContext
+    @Environment (\.dismiss) var dismiss
+    
+    @State private var name = ""
+    @State private var category = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section {
+                TextField("Create your own task", text: $name)
+                TextField("What category does this task belong in?", text: $category)
+                HStack {
+                    Spacer()
+                    Button ("Submit") {
+                        /*DataController().addPerson(username: <#T##String#>, password: <#T##String#>, name: <#T##String#>, categ: <#T##[String]#>, context: <#T##NSManagedObjectContext#>)
+                         */
+                        /*Datacontroller().addTask(priority: <#T##_Concurrency.TaskPriority?#>, operation: <#T##() -> ChildTaskResult#>)
+                         */
+                    }
+                    Spacer()
+                }
+            }
+        }
     }
 }
 
